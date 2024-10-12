86a3c6ad6164f9400552ff28401a281d0f31a792
Ray Sousa <raysousaml@gmail.com>
2024-10-12 11:44:25 -0300

    Configurações sobre a URLS

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
-# Create your views here.
+
 def home(request):
-    return HttpResponse('HOME 2')
+    return render(request, 'recipes/home.html')
 
 
 def contato(request):
